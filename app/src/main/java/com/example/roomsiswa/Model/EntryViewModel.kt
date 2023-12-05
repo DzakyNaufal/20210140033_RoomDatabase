ba781c22fc3b179ed3a7cd94137368e6d1efd0b9
DzakyNaufal <dzakynmulna@gmail.com>
2023-12-05 21:54:18 +0700

    menambahkan function DetailSiswa,Siswa.toUIStateSiswa, Siswa.toDetailSiswa

--- app/src/main/java/com/example/roomsiswa/Model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/Model/EntryViewModel.kt
@@ -49,3 +49,23 @@ data class  DetailSiswa(
     val alamat: String = "",
     val telpon: String = "",
 )
+
+/* Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis daanya */
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun  Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
