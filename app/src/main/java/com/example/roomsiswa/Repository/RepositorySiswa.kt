b4b1581973167c63dc85e549d502a34e94064469
DzakyNaufal <dzakynmulna@gmail.com>
2023-12-05 19:38:16 +0700

    membuat file RepositorySiswa dan membuat function getallsiswasteram, getsiswastream,insertsiswa,deletesiswa,updatesiswa

---
+++ app/src/main/java/com/example/roomsiswa/Repository/RepositorySiswa.kt
@@ -0,0 +1,12 @@
+package com.example.roomsiswa.Repository
+
+import com.example.roomsiswa.Data.Siswa
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun insertSiswa(siswa: Siswa)
+    suspend fun deleteSiswa(siswa: Siswa)
+    suspend fun updateSiswa(siswa: Siswa)
+}
