81ff3c3f7a95a22ca07c6be77baa2a5ae938a713
DzakyNaufal <dzakynmulna@gmail.com>
2023-12-06 17:23:47 +0700

    membuat fun listSiswa

--- app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanHome.kt
@@ -1,9 +1,11 @@
 package com.example.roomsiswa.ui.theme.Halaman
 
+import androidx.compose.foundation.FixedMotionDurationScale.key
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -98,6 +100,22 @@ fun BodyHome(
                 modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small))
             )
         }
+    }
+}
 
+@Composable
+fun listSiswa(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier = Modifier
+){
+    LazyColumn(modifier = Modifier){
+        items(items = itemSiswa, key = {it.id}){
+                person ->
+            DataSiswa(
+                siswa = person,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small))
+            )
+        }
     }
 }
