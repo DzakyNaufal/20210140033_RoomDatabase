c55e18bb2a1279356e1920cb921e54ae5a213c89
DzakyNaufal <dzakynmulna@gmail.com>
2023-12-06 10:10:58 +0700

    membuat variabel val homeUiState

--- app/src/main/java/com/example/roomsiswa/Model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/Model/HomeViewModel.kt
@@ -1,11 +1,23 @@
 package com.example.roomsiswa.Model
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.roomsiswa.Repository.RepositoriSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 
-class HomeViewModel(private val RepositorySiswa: RepositoriSiswa, it: Any): ViewModel(){
+class HomeViewModel(private val RepositorySiswa: RepositoriSiswa, it: Any): ViewModel() {
 
-    companion object{
+    companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+
+    val homeUiState: StateFlow<HomeUiState> = RepositoriSiswa.getAllSiswaStream.filterNotNull()
+
+        .map { HomeUiState(listSiswa = it.toList()) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState()
+        )
 }
