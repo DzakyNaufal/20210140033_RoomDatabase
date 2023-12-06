c859a2351a0d64cb1387318188fa1e50d62014bb
DzakyNaufal <dzakynmulna@gmail.com>
2023-12-06 10:23:57 +0700

    membuat class HomeViewModel dan membuat objek companion

---
+++ app/src/main/java/com/example/roomsiswa/Model/HomeModelView.kt
@@ -0,0 +1,14 @@
+package com.example.roomsiswa.Model
+
+import androidx.lifecycle.ViewModel
+
+import com.example.roomsiswa.Repository.RepositoriSiswa
+
+
+class HomeViewModel(private val RepositoriSiswa: RepositoriSiswa, it: Any): ViewModel() {
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+}
